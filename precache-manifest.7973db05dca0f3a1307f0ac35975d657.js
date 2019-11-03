636061f8aa3fa08ddb1c324c4c8756c01a68a7fc
creewick <ilia_Y@mail.ru>
2019-11-03 15:57:54 +0500

    Updates

---
+++ precache-manifest.7973db05dca0f3a1307f0ac35975d657.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4cc4e2dced94c05e72e4967252ad77f6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f5cb0df08552e3bb0a67",
+    "url": "/static/css/main.a25b803e.chunk.css"
+  },
+  {
+    "revision": "84fcdecfb37b3298ceee",
+    "url": "/static/js/2.2f4935dd.chunk.js"
+  },
+  {
+    "revision": "f5cb0df08552e3bb0a67",
+    "url": "/static/js/main.97906453.chunk.js"
+  },
+  {
+    "revision": "e8d152c8a5bb8527c948",
+    "url": "/static/js/runtime-main.9b7356be.js"
+  }
+]);
